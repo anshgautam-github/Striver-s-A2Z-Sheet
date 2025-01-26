f0d8dd038a14b545ba0f326e36e46176f77c7590
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-26 23:47:13 +0530

    Create 01-Pascal's Triangle.java

---
+++ 01- Array/03-Hard/01-Pascal's Triangle.java
@@ -0,0 +1,24 @@
+class Solution {
+
+     public static List<Integer> generateRow(int row) {
+        int ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1); //inserting the 1st element
+
+        //calculate the rest of the elements:
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.add((int)ans);
+        }
+        return ansRow;
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+          for (int row = 1; row <= numRows; row++) {
+            ans.add(generateRow(row));
+        }
+        return ans;
+    }
+}
